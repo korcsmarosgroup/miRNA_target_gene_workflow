3453c9ef4d8e743639e9dcb63db6699415fb9ff5
Dezso Modos <dezso.modos@netbiol.elte.hu>
2022-01-18 20:18:01 +0000

    From GEO2R added the mrNA analysisi iusing a simple limma package

--- workflow_code_detailed.R
+++ workflow_code_detailed.R
@@ -8,6 +8,11 @@ library("igraph")
 library("ggplot2")
 organism = "org.Hs.eg.db"
 library(organism, character.only = TRUE)
+library("data.table")
+library(limma)
+library(umap)
+
+
 
 # STEP 1: Choosing and checking the currated miRNA-target database 
 # Due to limatation of data avaliblty the miTarBase database is selcted for use.
@@ -111,6 +116,8 @@ version=checkMiRNAVersion(mitarbase_miRNANames, verbose = TRUE)
 #Now we can convert the two set of IDs to miRbase accesion numbers.
 TS_miRNA_acession = miRNA_NameToAccession(TS_miRNANames,version = "v21")
 TS_merged$Accession <- TS_miRNA_acession$Accession
+TS_miRNA_acession <- miRNA_NameToAccession(TS_context_selected$miRNA, version = "v21")
+TS_context_selected$Accession <- TS_miRNA_acession$Accession
 mitarbase_miRNA_acession = miRNA_NameToAccession(mitarbase_miRNANames,version = "v21")
 mitarbase_data$Accession <-  mitarbase_miRNA_acession$Accession
 # We can add the representative tables the miRNA acessions. 
@@ -118,9 +125,92 @@ mitarbase_data$Accession <-  mitarbase_miRNA_acession$Accession
 # STEP 4: Human ID mapping
 #For simplicity we can map to ENTREZ GENE ID both dataset.
 # ENSMEBLE mapping is hard due to version inconsistencies
-organism = "org.Hs.eg.db"
-library(organism, character.only = TRUE)
-entrez_gene_mapping <- AnnotationDbi::select(org.Hs.eg.db, keys=TS_merged$Gene.Symbol, columns='ENTREZID', keytype='SYMBOL')
-TS_all <- merge(TS_merged, entrez_gene_mapping, by.x = "Gene.Symbol", by.y = "SYMBOL")
+entrez_gene_mapping <- AnnotationDbi::select(org.Hs.eg.db, keys=unique(TS_merged$Gene.Symbol), columns='ENTREZID', keytype='SYMBOL')
+entrez_gene_mapping_TS <- AnnotationDbi::select(org.Hs.eg.db, keys=unique(TS_context_selected$Gene.Symbol), columns='ENTREZID', keytype='SYMBOL')
 
-#STEP 5:  
+# Converting the data.frame object to data.tabe objects for speed.
+TS_merged <- data.table(TS_merged)
+entrez_gene_mapping <- data.table(entrez_gene_mapping)
+TS_all <- merge(TS_merged, entrez_gene_mapping, by.x = "Gene.Symbol", by.y = "SYMBOL", allow.cartesian=TRUE)
+dim(TS_all)
+head(TS_all, n=30)
+TS_context_selected <- data.table(TS_context_selected)
+entrez_gene_mapping_TS <- data.table(entrez_gene_mapping_TS)
+TS_context_all <- merge(TS_context_selected, entrez_gene_mapping_TS, by.x = "Gene.Symbol", by.y = "SYMBOL", allow.cartesian=TRUE)
+
+#STEP 5: Analysing the transcriptomic data. First the transcriptomic data.
+# load series and platform data from GEO
+gset <- getGEO("GSE102127", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL6244", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+# log2 transformation
+# group membership for all samples
+gsms <- paste0("000000000000XXX11111111111111XXXXXXXXXXXXXXXXXXXXX",
+               "XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+sml <- strsplit(gsms, split="")[[1]]
+
+# filter out excluded samples (marked as "X")
+sel <- which(sml != "X")
+sml <- sml[sel]
+gset <- gset[ ,sel]
+
+# log2 transformation
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+    (qx[6]-qx[1] > 50 && qx[2] > 0)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# assign samples to groups and set up design matrix
+gs <- factor(sml)
+groups <- make.names(c("control","late_stage_CD"))
+levels(gs) <- groups
+gset$group <- gs
+design <- model.matrix(~group + 0, gset)
+colnames(design) <- levels(gs)
+
+fit <- lmFit(gset, design)  # fit linear model
+
+# set up contrasts of interest and recalculate model coefficients
+cts <- paste(groups[1], groups[2], sep="-")
+cont.matrix <- makeContrasts(contrasts=cts, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+
+# compute statistics and table of top significant genes
+fit2 <- eBayes(fit2, 0.01)
+tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
+
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title"))
+write.table(tT, file=stdout(), row.names=F, sep="\t")
+
+# Visualize and quality control test results.
+# Build histogram of P-values for all genes. Normal test
+# assumption is that most genes are not differentially expressed.
+tT2 <- topTable(fit2, adjust="fdr", sort.by="B", number=Inf)
+hist(tT2$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+     ylab = "Number of genes", main = "P-adj value distribution")
+
+# summarize test results as "up", "down" or "not expressed"
+dT <- decideTests(fit2, adjust.method="fdr", p.value=0.05)
+
+# Venn diagram of results
+vennDiagram(dT, circle.col=palette())
+
+# create Q-Q plot for t-statistic
+t.good <- which(!is.na(fit2$F)) # filter out bad probes
+qqt(fit2$t[t.good], fit2$df.total[t.good], main="Moderated t statistic")
+
+# volcano plot (log P-value vs log fold change)
+colnames(fit2) # list contrast names
+ct <- 1        # choose contrast of interest
+volcanoplot(fit2, coef=ct, main=colnames(fit2)[ct], pch=20,
+            highlight=length(which(dT[,ct]!=0)), names=rep('+', nrow(fit2)))
+
+# MD plot (log fold change vs mean log expression)
+# highlight statistically significant (p-adj < 0.05) probes
+plotMD(fit2, column=ct, status=dT[,ct], legend=F, pch=20, cex=1)
+abline(h=0)
