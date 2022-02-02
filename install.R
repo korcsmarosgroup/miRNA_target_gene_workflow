0846b7f822e8af9ef0bd9c5ac928cd24f25989df
Dezso Modos <dezso.modos@netbiol.elte.hu>
2022-02-02 10:32:04 +0000

    Adding

--- install.R
+++ install.R
@@ -7,12 +7,18 @@ BiocManager::install("miRBaseConverter")
 BiocManager::install("AnnotationDbi")
 BiocManager::install("GEOquery")
 BiocManager::install("affy")
-BiocManager::install("miRLAB")
 BiocManager::install("clusterProfiler")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("limma")
 BiocManager::install("umap")
+BiocManager::install("ggpubr")
 
 install.packages("igraph")
 install.packages("ggplot2")
 install.packages("data.table")
+
+#mirlab isntallation function is not working from bioconductor due to previous pakages 
+# are out of date.
+library(devtools)
+install_github('pvvhoang/miRLAB')
+
